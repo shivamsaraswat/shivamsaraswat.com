382d03b644494626b8219adccdae393ab4fc6c1b
Shivam Saraswat <44198501+shivamsaraswat@users.noreply.github.com>
2024-05-30 17:50:08 +0000

    Update image name

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -180,7 +180,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpeg"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
